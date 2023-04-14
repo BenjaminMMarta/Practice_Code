9b46e8c3c47ba1f177bc1c7babcc9bd2757f7e5e
Benjamin M. Marta <108314514+BenjaminMMarta@users.noreply.github.com>
2023-04-14 12:35:43 -0700

    Added app.js and index.html files

---
+++ app.js
@@ -0,0 +1,23 @@
+let numbers = [1,10,3,7,5,9,2,8,4,6];
+let newNumbers = [];
+
+function loop() {
+  /*for (let i = 0; i <= numbers.length -1; i++) {
+    newNumbers.push(`The number ${numbers[i]} was added!`);
+    console.log(newNumbers); 
+  }*/
+  for (let i = 0; i <= numbers.length -1; i++) {
+    newNumbers.push(numbers[i]);
+    console.log(`The number ${numbers[i]} was added to the NewNumbers array!`); 
+  }
+}
+
+loop();
+
+let names = ["Beck", "Zebra", "Axel", "Stephen", "John"];
+names.sort();
+console.log(names);
+     
+let moreNumbers = [5,8,2,1,6,3];
+moreNumbers.sort();
+console.log(moreNumbers);
