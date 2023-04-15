7a5cf4da8919b8fd0b34595c7c7f86384bb89c9f
Benjamin M. Marta <108314514+BenjaminMMarta@users.noreply.github.com>
2023-04-14 17:48:59 -0700

    Added changes to app.js file

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-let numbers = [1,10,3,7,5,9,2,8,4,6];
+/*let numbers = [1,10,3,7,5,9,2,8,4,6];
 let newNumbers = [];
 
 function loop() {
   /*for (let i = 0; i <= numbers.length -1; i++) {
     newNumbers.push(`The number ${numbers[i]} was added!`);
     console.log(newNumbers); 
-  }*/
+  }
   for (let i = 0; i <= numbers.length -1; i++) {
     newNumbers.push(numbers[i]);
     console.log(`The number ${numbers[i]} was added to the NewNumbers array!`); 
   }
-}
+  */
 
 loop();
 
@@ -21,3 +21,9 @@ console.log(names);
 let moreNumbers = [5,8,2,1,6,3];
 moreNumbers.sort();
 console.log(moreNumbers);
+
+function saying(){
+  console.log("I'm a saying and I say it all day!");
+}
+
+saying();
