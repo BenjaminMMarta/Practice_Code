2f107ed817741074c4e96e95e124495fceab1c2c
Benjamin M. Marta <108314514+BenjaminMMarta@users.noreply.github.com>
2023-04-14 18:40:00 -0700

    Added more to the app.js file.

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
-/*let numbers = [1,10,3,7,5,9,2,8,4,6];
+// Loop Practice //
+
+
+/*
+let numbers = [1,10,3,7,5,9,2,8,4,6];
 let newNumbers = [];
 
 function loop() {
-  /*for (let i = 0; i <= numbers.length -1; i++) {
+  for (let i = 0; i <= numbers.length -1; i++) {
     newNumbers.push(`The number ${numbers[i]} was added!`);
     console.log(newNumbers); 
-  }
-  for (let i = 0; i <= numbers.length -1; i++) {
-    newNumbers.push(numbers[i]);
-    console.log(`The number ${numbers[i]} was added to the NewNumbers array!`); 
-  }
-
+  } 
+}
 
 loop();
+*/
 
+
+// Array Practice //
+
+
+/*
 let names = ["Beck", "Zebra", "Axel", "Stephen", "John"];
 names.sort();
 console.log(names);
@@ -22,6 +28,10 @@ let moreNumbers = [5,8,2,1,6,3];
 moreNumbers.sort();
 console.log(moreNumbers);
 */
+
+
+// Function & Conditional Statement Practice //
+
 
 function saying(food) {
   if (food === "Pizza" || food === "pizza") {
